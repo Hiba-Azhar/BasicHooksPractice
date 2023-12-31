6da9ed8bd12945712e9905170d5a752c691e4f68
Hiba <yusraazhar2002@gmail.com>
2023-12-31 17:50:16 +0500

    Hooks ID buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  var [count, setCount] = useState(0)
+  function increase() {
+    setCount(count + 1)
+  }
+  function decrease() {
+    setCount(count - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>{count}</h1>
+      <button onClick={increase}>
+        Click me to increase the state of the text above
+      </button>
+      <button onClick={decrease}>
+        Click me to decrease the state of the text above
+      </button>
+    </>
+  )
 }
 
-export default App;
+export default App
